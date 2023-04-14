16d9b6a2338cbbd253c7fd8c910a68cd758ac22c
Sergej Breiter <Sergej.Breiter@nm.ifi.lmu.de>
2023-04-14 18:22:38 +0200

    fix register value setting, add test/example code and restore functionality

--- a64fx_prf_dst.h
+++ a64fx_prf_dst.h
@@ -1,4 +1,4 @@
-// #pragma once
+#pragma once
 
 #include "stddef.h"
 #include "stdio.h"
@@ -18,6 +18,21 @@ static const size_t BITS_PER_LONG_LONG = 8 * sizeof(long long);
 #define GENMASK_ULL(h, l)                \
     (((~ULL(0)) - (ULL(1) << (l)) + 1) & \
      (~ULL(0) >> (BITS_PER_LONG_LONG - 1 - (h))))
+
+#define print_bits(x)                                      \
+    do {                                                   \
+        typeof(x) a__ = (x);                               \
+        char *p__ = (char *)&a__ + sizeof(x) - 1;          \
+        size_t bytes__ = sizeof(x);                        \
+        printf(#x ": ");                                   \
+        while (bytes__--) {                                \
+            char bits__ = 8;                               \
+            while (bits__--)                               \
+                putchar(*p__ & (1ULL << bits__) ? '1' : '0'); \
+            p__--;                                         \
+        }                                                  \
+        putchar('\n');                                     \
+    } while (0)
 
 // IMP_PF_CTRL_EL1 : S3_0_C11_C4_0
 // set bit 62-63 makes IMP_PF_STREAM_DETECT_CTRL_EL0 accessible from EL0
@@ -45,6 +60,17 @@ __asm__ volatile(\
             );\
 }
 
+#define A64FX_DISABLE_PF_CTRL() \
+{\
+    uint64_t val = 0ULL; \
+__asm__ volatile(\
+            "msr S3_0_C11_C4_0,%[val]"\
+            :\
+            : [val] "r" (val)\
+            :\
+            );\
+}
+
 
 // The distance of the hardware prefetch to L1 cache is specified.
 // The prefetch distance for L1 is calculated as (L1_DIST * 256B).
@@ -55,18 +81,18 @@ __asm__ volatile(\
 // set the l1 prefetch distance to @dist
 #define A64FX_SET_PF_DST_L1(orig, dist) \
 {\
-__asm__ volatile(\
-        "mrs %["#orig"],S3_3_C11_C4_0"\
-        : [orig] "=r" (orig)\
-        : \
-        :\
-        ); \
+    __asm__ volatile(\
+            "mrs %["#orig"],S3_3_C11_C4_0"\
+            : [orig] "=r" (orig)\
+            : \
+            :\
+            ); \
     uint64_t distu64 = dist; \
     uint64_t val = orig; \
     uint64_t mask = ~GENMASK_ULL(27, 24); \
     val = val & mask; \
-    val = val & (distu64 << 24); \
-__asm__ volatile(\
+    val = val | (distu64 << 24) | (1ULL << 63); \
+    __asm__ volatile(\
             "msr S3_3_C11_C4_0,%[val]"\
             :\
             : [val] "r" (val)\
@@ -83,21 +109,88 @@ __asm__ volatile(\
 // set the l1 prefetch distance to @dist
 #define A64FX_SET_PF_DST_L2(orig, dist) \
 {\
-__asm__ volatile(\
-        "mrs %["#orig"],S3_3_C11_C4_0"\
-        : [orig] "=r" (orig)\
-        : \
-        :\
-        ); \
+    __asm__ volatile(\
+            "mrs %["#orig"],S3_3_C11_C4_0"\
+            : [orig] "=r" (orig)\
+            : \
+            :\
+            ); \
     uint64_t distu64 = dist; \
     uint64_t val = orig; \
     uint64_t mask = ~GENMASK_ULL(19, 16); \
     val = val & mask; \
-    val = val & (distu64 << 16); \
-__asm__ volatile(\
+    val = val | (distu64 << 16) | (1ULL << 63); \
+    __asm__ volatile(\
             "msr S3_3_C11_C4_0,%[val]"\
             :\
             : [val] "r" (val)\
             :\
             );\
 }
+
+// reads the register value
+#define A64FX_READ_PF_DST(orig) \
+{\
+    __asm__ volatile(\
+            "mrs %["#orig"],S3_3_C11_C4_0"\
+            : [orig] "=r" (orig)\
+            : \
+            :\
+            ); \
+}
+
+// sets the register value
+#define A64FX_WRITE_PF_DST(regval) \
+{\
+    uint64_t val = regval; \
+    __asm__ volatile(\
+            "msr S3_3_C11_C4_0,%[val]"\
+            :\
+            : [val] "r" (val)\
+            :\
+            );\
+}
+
+#if 0
+// test code and example usage
+int main() {
+        printf("old register values:\n");
+#pragma omp parallel
+        {
+        // A64FX_ENABLE_PF_CTRL(); // only accessible from EL1
+        uint64_t orig;
+        A64FX_READ_PF_DST(orig); // first read the original value
+#pragma omp critical
+        print_bits(orig); // should print only zeroes
+
+        uint64_t tmp;
+        A64FX_SET_PF_DST_L1(tmp, 3);
+        A64FX_SET_PF_DST_L2(tmp, 7);
+
+        A64FX_READ_PF_DST(tmp);
+
+#pragma omp barrier
+#pragma omp single
+        printf("new register values:\n");
+#pragma omp barrier
+
+#pragma omp critical
+        print_bits(tmp); // should print also ones
+
+        A64FX_WRITE_PF_DST(orig);
+        A64FX_READ_PF_DST(tmp); // first read the original value
+
+#pragma omp barrier
+#pragma omp single
+        printf("reset register values:\n");
+#pragma omp barrier
+
+
+#pragma omp critical
+        print_bits(tmp); // should print only zeroes
+
+        // A64FX_DISABLE_PF_CTRL(); // only accessible from EL1
+        }
+}
+
+#endif
